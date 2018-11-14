0dd536777736b0ec65788383158c07721942ebb4
Patrick Wegerer <patrick.wegerer@gmail.com>
2018-11-14 22:12:09 +0100

    Finished 2

--- app.js
+++ app.js
@@ -2,29 +2,46 @@
 //$(document).on("load, change", function(){
 //$(document).ready(function(){
 //    $(document).change(function(){  //Ändern wenn Seite lädt?   
-$(document).on("load change", function () {
+var correct;
+$("#otherMediaChannel").hide();
+$("#emailMandatory").hide();
+$("#submit").hide(); //Am Anfang hiden, weil es bei load aus irgendeinem Grund nicht hided ist von Anfang an
+$(document).on("ready load change", function () {
+    correct = true;
     if ($("#firstName").val() == "") {
         $("#firstNameMandatory").show();
+        correct = false;
     }
     else {
         $("#firstNameMandatory").hide();
     }
     if ($("#lastName").val() == "") {
         $("#lastNameMandatory").show();
+        correct = false;
     }
     else {
         $("#lastNameMandatory").hide();
     }
     if ($("#newsletter").prop('checked') && $("#email").val() == "") {
         $("#emailMandatory").show();
+        correct = false;
     }
     else {
         $("#emailMandatory").hide();
     }
     if ($("#mediaChannelSelect option:selected").text() == "Other") {
         $("#otherMediaChannel").show();
+        if ($("#otherMediaChannel").val() == "") {
+            correct = false;
+        }
     }
     else {
         $("#otherMediaChannel").hide();
+    }
+    if (correct) {
+        $("#submit").show();
+    }
+    else {
+        $("#submit").hide();
     }
 });
--- app.ts
+++ app.ts
@@ -3,31 +3,46 @@
 //$(document).on("load, change", function(){
 //$(document).ready(function(){
 //    $(document).change(function(){  //Ändern wenn Seite lädt?   
-
-$(document).on("load change", function(){
+let correct: boolean;
+$("#otherMediaChannel").hide();
+$("#emailMandatory").hide();
+$("#submit").hide(); //Am Anfang hiden, weil es bei load aus irgendeinem Grund nicht hided ist von Anfang an
+$(document).on("ready load change", function(){
+    correct=true;
     if($("#firstName").val()==""){
-        $("#firstNameMandatory").show(); 
+        $("#firstNameMandatory").show();
+        correct=false; 
     }else{ 
         $("#firstNameMandatory").hide();   
     }
 
     if($("#lastName").val()==""){
-        $("#lastNameMandatory").show(); 
+        $("#lastNameMandatory").show();
+        correct=false; 
     }else{ 
         $("#lastNameMandatory").hide();     
     }
     
     if($("#newsletter").prop('checked')&&$("#email").val()==""){
         $("#emailMandatory").show();
+        correct=false; 
     }else{
         $("#emailMandatory").hide();
     }
 
     if($("#mediaChannelSelect option:selected").text()=="Other"){
         $("#otherMediaChannel").show();
+        if($("#otherMediaChannel").val()==""){
+            correct=false; 
+        }
     }else{
         $("#otherMediaChannel").hide();
-    }            
+    }
+    if(correct){
+        $("#submit").show();    
+    }else{
+        $("#submit").hide(); 
+    }
 });
 
        
